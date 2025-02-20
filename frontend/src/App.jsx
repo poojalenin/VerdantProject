2ba2b72b3e698119b09cc6fe3e3423069ebbf874
poojalenin69 <poojalenin69@gmail.com>
2025-02-20 15:52:21 +0530

    backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Collection from './pages/Collection';
 import About from './pages/About';
 import Contact from './pages/Contact';
+
 import Product from './pages/Product';
 import Cart from './pages/Cart';
 import Footer from './components/Footer';
